37747baf95379ce546959ae1c67a37ff3484c7f9
Sukhjeet Gill <sukhjeetgill21800@gmail.com>
2024-01-23 23:25:42 -0500

    final

--- src/JavaStreams/OddIndex.java
+++ src/JavaStreams/OddIndex.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class OddIndex {
     public static void main(String[] args) {
-        List<Integer> integers = Arrays.asList(2, 4, 6, 8, 9, 10, 12, 14, 16);
+        List<Integer> integers = Arrays.asList(2, 4, 6, 8,9,12, 10, 12, 14, 16);
         int n = integers.indexOf(integers.stream()
-                .filter(i -> i%2 !=0)
+                .filter(i -> i % 2 != 0)
                 .findFirst()
+//                .orElseThrow(() -> new RuntimeException("No odd number found")));
                 .orElse(0));
-
-        System.out.println("Index of first occurence of odd: " + n);
+        System.out.println("Index of first occurrence of odd: " + n);
         List <Integer> integerList = integers.subList(n+1,integers.size());
         System.out.println(integerList);
 
